e94c84ab4020eb28d6359acef7fb3da1ae96f85e
Todor Arnaudov <twenkid@gmail.com>
2021-12-13 21:13:36 +0200

    Delete install.bat

--- version/v5/install.bat
+++
@@ -1,5 +0,0 @@
-python -m pip install flask-wtf
-python -m pip install flask-sqlalchemy
-python -m pip install flask-migrate
-python -m pip install flask-login
-python -m pip install email-validator
