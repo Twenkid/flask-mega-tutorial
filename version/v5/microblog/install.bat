dbe088ffd90f7f034781b1677cc2fd95dd5863df
Todor Arnaudov <twenkid@gmail.com>
2021-12-13 21:11:32 +0200

    Fixed ... login .. pass > 1 symb ... aaaa 1111

---
+++ version/v5/microblog/install.bat
@@ -0,0 +1,5 @@
+python -m pip install flask-wtf
+python -m pip install flask-sqlalchemy
+python -m pip install flask-migrate
+python -m pip install flask-login
+python -m pip install email-validator
