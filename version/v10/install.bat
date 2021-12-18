697b2cf06073c15447d6c7d411fabfbb495c4310
Todor Arnaudov <twenkid@gmail.com>
2021-12-19 00:44:17 +0200

    V10 - Email Reset Password ... tokens jwt
    ResetPasswordRequestForm ,,,

---
+++ version/v10/install.bat
@@ -0,0 +1,9 @@
+python -m pip install flask-wtf
+python -m pip install flask-sqlalchemy
+python -m pip install flask-migrate
+python -m pip install flask-login
+python -m pip install email-validator
+python -m pip install pyjwt
+python -m pip install flask-mail
+
+C:\Windows\system32>
