a07d857d2ec4ad07917f38ff3c05b4c809c537c5
Todor Arnaudov <twenkid@gmail.com>
2021-12-21 00:53:57 +0200

    v14

---
+++ version/v14/microblog/config.py
@@ -0,0 +1,28 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    print("SQLALCHEMY_DATABASE_URI=",SQLALCHEMY_DATABASE_URI)
+    
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['your-email@example.com']
+    POSTS_PER_PAGE = 10
+    LANGUAGES = ['en', 'es'] #['es'] #['en', 'es'] #'bg', 'es'] 'es' - only about time passed ... "tosh said hace un día"
+    MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
+    
+'''
+import os
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    
+'''   
---
+++ version/v14/microblog/install.bat
@@ -0,0 +1,15 @@
+python -m pip install flask-wtf
+python -m pip install flask-sqlalchemy
+python -m pip install flask-migrate
+python -m pip install flask-login
+python -m pip install email-validator
+python -m pip install pyjwt
+python -m pip install flask-mail
+python -m pip install flask-moment
+python -m pip install flask-bootstrap
+python -m pip install flask-babel
+python -m pip install requests
+python -m pip install langdetect
+python -m pip install elasticsearch-async
+
+C:\Windows\system32>
